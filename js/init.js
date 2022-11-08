00ce52d1b07dcf79db0990bae1ee6538f4adbdf2
autenticahhm1 <117742691+autenticahhm1@users.noreply.github.com>
2022-11-08 08:49:31 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDAtZGTB7xgRBR8F9dKA7ttrnw8_Pqp15I",
+  authDomain: "autenticahhm1.firebaseapp.com",
+  projectId: "autenticahhm1",
+  storageBucket: "autenticahhm1.appspot.com",
+  messagingSenderId: "731354335554",
+  appId: "1:731354335554:web:a95195414ee7263e02e0db",
+  measurementId: "G-B555K9DJSK"
+    });
